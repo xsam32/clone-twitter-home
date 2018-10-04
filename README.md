02cd5489d6641e2134ca9a3b5066e5efad2de423
xsam32 <xpvampire_sx@hotmail.com>
2018-10-04 09:58:01 +0700

    Start clone-twitter

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# clone-twitter-home
+# twitter_home
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/models/post_model.dart
@@ -0,0 +1,29 @@
+class PostModel {
+  String profileImageURL;
+  String userName;
+  String accountName;
+  String time;
+  String postDetail;
+  String postImageURL1;
+  String postImageURL2;
+  String postImageURL3;
+  String postImageURL4;
+  String countComment;
+  String countReTwitt;
+  String countLike;
+
+  PostModel(
+    this.profileImageURL,
+    this.userName,
+    this.accountName,
+    this.time,
+    this.postDetail,
+    this.postImageURL1,
+    this.postImageURL2,
+    this.postImageURL3,
+    this.postImageURL4,
+    this.countComment,
+    this.countReTwitt,
+    this.countLike,
+  );
+}
